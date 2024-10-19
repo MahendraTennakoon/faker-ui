c880827516dbb6a91fb66e22814956eda7fa2705
Mahendra Tennakoon <mahendrathennakoon@gmail.com>
2024-10-19 17:19:57 +1100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { fakerEN_AU as faker } from "@faker-js/faker";
+import { Person } from "./types";
+import { createRandomPerson } from "./factory";
+
+function App() {
+  const [person, setPerson] = useState<Person | null>(null);
+  useEffect(() => {
+    setPerson(createRandomPerson());
+  }, []);
+  if (!person) return null;
+
+  return (
+    <>
+      <h1>Faker UI</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Mobile (Au)</th>
+            <th>Email</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>{person.firstName}</td>
+            <td>{person.lastName}</td>
+            <td>{person.phoneMobile}</td>
+            <td>{person.email}</td>
+          </tr>
+        </tbody>
+      </table>
+    </>
+  );
+}
+
+export default App;
---
+++ src/factory.ts
@@ -0,0 +1,14 @@
+import { fakerEN_AU as faker } from "@faker-js/faker";
+import { Person } from "./types";
+
+export const createRandomPerson = (): Person => {
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+
+  return {
+    firstName,
+    lastName,
+    email: faker.internet.email({ firstName, lastName }),
+    phoneMobile: faker.phone.number(),
+  };
+};
